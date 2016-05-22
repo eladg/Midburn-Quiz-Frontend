76ae07b39885b0bd4468524cf8f13e6a06ddf7ff
eladg <ggrelad@gmail.com>
2016-05-21 21:59:11 -0700

    load configs from config.json

---
+++ config.js
@@ -0,0 +1,13 @@
+getConfigFromConfigFile = function(){
+  var _config;
+  $.ajax({
+    url: 'config.json',
+    success: function (data) {
+      _config = data;
+    },
+    async: false,
+    dataType: 'json'
+  });
+
+  return _config;
+};
